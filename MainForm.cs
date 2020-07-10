b5dddf56fac2e5463d79ce00312da869056a4e2c
Acherons_ <kamron.cole13@gmail.com>
2020-07-10 19:24:00 -0400

    Initial commit

---
+++ MainForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace UmbraInjector
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            isProcessRunning.Start();
+        }
+
+        private void IsProcessRunning_Tick(object sender, EventArgs e)
+        {
+            Process[] getProcess = Process.GetProcessesByName("Risk of Rain 2");
+            if (getProcess.Length != 0)
+            {
+                isProcessRunning.Stop();
+                Inject(false);
+                Application.Exit();
+            }
+        }
+
+        public static void Inject(bool alreadyOpen)
+        {
+            if (!alreadyOpen)
+            {
+                Thread.Sleep(15000);
+            }
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = $"/C cd Data&smi.exe inject -p \"Risk of Rain 2\" -a UmbraRoR-v1.2.4.dll -n UmbraRoR -c Loader -m Load";
+            process.StartInfo = startInfo;
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace UmbraInjector
+{
+    static class Program
+    {
+        public static bool getProcessFirstTry;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Process[] getProcess = Process.GetProcessesByName("Risk of Rain 2");
+            getProcessFirstTry = getProcess.Length != 0;
+
+            if (!getProcessFirstTry)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
+            else
+            {
+                MainForm.Inject(true);
+            }
+        }
+    }
+}
