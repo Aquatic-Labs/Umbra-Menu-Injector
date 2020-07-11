b3733a5f81c9db00605d986fdf28b552cb541578
Acherons_ <kamron.cole13@gmail.com>
2020-07-11 03:13:31 -0400

    Update MainForm.Designer.cs

--- source/MainForm.Designer.cs
+++ source/MainForm.Designer.cs
@@ -106,6 +106,7 @@
             this.Controls.Add(this.Title);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "Umbra Injector";
             this.Load += new System.EventHandler(this.MainForm_Load);
